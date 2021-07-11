b6c47e16f6539cc401a9388004c0f099c8b9a41f
rohitmadrileno15 <rohitmadrileno@gmail.com>
2021-07-11 14:10:12 +0000

    Added

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,50 @@
-import React from 'react'
+ 
+import { useCart } from 'cart-react-ecommerce'
 
-import { ExampleComponent } from 'cart-react-ecommerce'
+import React, { useState, useEffect } from "react"
 import 'cart-react-ecommerce/dist/index.css'
 
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const [all, setState] = useState([])
+
+  const p = new useCart( )
+
+  p.addItem( "Watch" , 1200 , 1 , {'manufacturer' : 'Adidas'})
+  p.addItem( "Shirt" , 1200 , 1 , {'manufacturer' : 'Adidas'})
+  
+  //updates the quantity
+  p.updateItem("Shirt")
+
+
+
+    const allItems = () => { p.getAll().then (
+      (r) => {
+        setState(r)
+      }
+      )}
+
+      useEffect(()=>{
+        allItems()
+      },[]);
+ 
+
+  return (
+    <div>
+      <h1>React App</h1>
+      <br/>
+            <ul>
+              {all.map( data => (
+              <li>
+              {data.name} &nbsp; quantity of {data.quantity}
+             </li>
+              ))}
+
+            </ul>
+    </div>
+
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,114 @@
-import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import Dexie from 'dexie';
+ 
+
+class useCart{
+  constructor(   ){
+
+    // default currency value is INR
+    this.cartCurrency =  "INR" ;
+
+    this.db = new Dexie("CartDatabase");
+    this.db.version(1).stores({ items: "++id,name,price,quantity,others" });
+
+  }
+
+
+ updateItem( _name ) {
+    const modifyQuantity = ( _id , _q ) => {
+      this.db.items.update( _id , {quantity: 1 + _q })      
+    }
+   
+    var got = this.db.items.where("name").equalsIgnoreCase(_name).first(function(count) {
+      if(count != undefined) {
+        modifyQuantity(count.id , count.quantity)        
+        return true
+      }          
+    })  
+  }
+ 
+   
+  addItem( _name , _price , _quantity , _others){
+
+    this.db.items.where("name").equalsIgnoreCase(_name).first().then(
+      (r) => {
+        if(r === undefined) {
+          this.db.items.add({name: _name, price:  _price , quantity :  _quantity , others: _others})
+          
+        }
+        else{
+          this.updateItem(_name)
+        }
+      }) 
+    }
+
+
+  removeItem( _name ) {
+    this.db.items.where("name").equalsIgnoreCase(_name).first().then(
+      (r) => {
+        if(r === undefined) {
+         return 
+        }
+        else{
+          this.db.items.delete(r.id)
+        }
+      }) 
+  }
+
+  getTotalPrice () {    
+    var val_ = this.getAll().then(
+      (r) => {
+        var _totalPrice = 0
+        
+        for (let i = 0; i < r.length; i++) {
+          _totalPrice += r[i].price * r[i].quantity 
+        }
+        return _totalPrice
+      }
+    )
+    return val_
+  }
+
+  getCheckOutPrice (promo) {
+    if( typeof(promo) != 'number' ) {
+      throw 'Promo must be a number!'
+    }
+    var gotTotalPrice = this.getTotalPrice().then(
+      (r) => {  
+        const final_val = r-promo
+        return {"currency" : this.cartCurrency , 'grandTotal' : final_val}
+      }
+    )
+
+    return gotTotalPrice
+  }
+
+
+  async getAll() {
+      var gotall = await this.db.items.toArray()
+      return gotall
+    }
+
+
+  setCartClear() {
+      return this.db.items.clear()
+      
+    }
+
+  async getCartinReverse() {
+      var gotall = await this.db.items.reverse().toArray()
+      return gotall
+  }
+
+
+  setCartCurrency( currency ) {
+    this.cartCurrency = currency
+  }
+
 }
+  
+
+
+
+export { useCart }
+ 
